1bb920ec464ab4b70c6b20d16a817eb6629f5c9f
Peter Lucht <hape@quantenparallaxum.de>
2021-01-13 18:46:38 +0100

    refactor to higher order functions

--- src/clj8puzzle/state.clj
+++ src/clj8puzzle/state.clj
@@ -11,31 +11,28 @@
   [v i j]
   (assoc v i (v j) j (v i)))
 
-(defn move-left
-  "Returns a vector with '0' moved left one field or nil if '0' can't be moved."
-  [state]
-  (let [i (index-of state 0)]
-      (if (#{0 3 6} i) nil (swapv state i (dec i)))))
+(defn move 
+  "Returns a vector with '0' moved one field in direction or nil if '0' can't be moved." 
+  [direction state]
+  (direction state (index-of state 0)))
 
-(defn move-right
-  "Returns a vector with '0' moved right one field or nil if '0' can't be moved."
-  [state]
-  (let [i (index-of state 0)]
-      (if (#{2 5 8} i) nil (swapv state i (inc i)))))
+(defn left 
+  [state i] 
+  (if (#{0 3 6} i) nil (swapv state i (dec i))))
 
-(defn move-up
-  "Returns a vector with '0' moved up one field or nil if '0' can't be moved."
-  [state]
-  (let [i (index-of state 0)]
-      (if (#{0 1 2} i) nil (swapv state i (- i 3)))))
+(defn right 
+  [state i]
+  (if (#{2 5 8} i) nil (swapv state i (inc i))))
 
-(defn move-down
-  "Returns a vector with '0' moved down one field or nil if '0' can't be moved."
-  [state]
-  (let [i (index-of state 0)]
-      (if (#{6 7 8} i) nil (swapv state i (+ i 3)))))
+(defn up 
+  [state i]
+  (if (#{0 1 2} i) nil (swapv state i (- i 3))))
+
+(defn down
+  [state i]
+  (if (#{6 7 8} i) nil (swapv state i (+ i 3))))
 
 (defn generate-states
   "Returns a list of all possible next states for the current (given) state."
   [state]
-  (filter some? [(move-up state) (move-right state) (move-down state) (move-left state)]))
+  (filter some? [(move up state) (move right state) (move down state) (move left state)]))
