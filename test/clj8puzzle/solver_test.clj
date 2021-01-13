5aba754097921e988de2a05ea7fbb57ebf297ed6
Peter Lucht <hape@quantenparallaxum.de>
2021-01-13 23:27:32 +0100

    decorate test data

--- test/clj8puzzle/solver_test.clj
+++ test/clj8puzzle/solver_test.clj
@@ -4,11 +4,11 @@
 
 (deftest goal-test
   (testing "Goal test happy case."
-    (is (true? (goal? [1 2 3 4 5 6 7 8 0])))))
+    (is (true? (goal? [1 2 3, 4 5 6, 7 8 0])))))
 
 (deftest goal-test
   (testing "Goal test unhappy case."
-    (is (false? (goal? [1 2 3 4 5 6 7 0 8])))))
+    (is (false? (goal? [1 2 3, 4 5 6, 7 0 8])))))
 
 (deftest filter-unexplored-one-arg-test
   (testing "Only keep nodes which are not queue."
@@ -32,40 +32,40 @@
 
 (deftest initial-state-is-goal-test
   (testing "Initial state is already the goal."
-    (is (= [1 2 3 4 5 6 7 8 0] (first (solve [1 2 3 4 5 6 7 8 0]))))))
+    (is (= [1 2 3, 4 5 6, 7 8 0] (first (solve [1 2 3, 4 5 6, 7 8 0]))))))
 
 (deftest one-move-away-test
   (testing "Goal is one move away."
-    (is (= [1 2 3 4 5 6 7 8 0] (first (solve [1 2 3 4 5 6 7 0 8]))))
-    (is (= [1 2 3 4 5 6 7 8 0] (first (solve [1 2 3 4 5 0 7 8 6]))))))
+    (is (= [1 2 3, 4 5 6, 7 8 0] (first (solve [1 2 3, 4 5 6, 7 0 8]))))
+    (is (= [1 2 3, 4 5 6, 7 8 0] (first (solve [1 2 3, 4 5 0, 7 8 6]))))))
 
 (deftest two-moves-away-test
   (testing "Goal is two moves away."
-    (is (= [1 2 3 4 5 6 7 8 0] (first (solve [1 2 3 4 5 6 0 7 8]))))
-    (is (= [1 2 3 4 5 6 7 8 0] (first (solve [1 2 3 4 0 5 7 8 6]))))))
+    (is (= [1 2 3, 4 5 6, 7 8 0] (first (solve [1 2 3, 4 5 6, 0 7 8]))))
+    (is (= [1 2 3, 4 5 6, 7 8 0] (first (solve [1 2 3, 4 0 5, 7 8 6]))))))
 
 (deftest three-moves-away-test
   (testing "Goal is three moves away."
-    (is (= [1 2 3 4 5 6 7 8 0] (first (solve [1 2 3 0 4 5 7 8 6]))))))
+    (is (= [1 2 3, 4 5 6, 7 8 0] (first (solve [1 2 3 0 4 5 7 8 6]))))))
 
 (deftest ^:slow regression-1-test
   (testing "This test data provokes unexplored-nodes to be nil at some point.
             The solution space must not be updated with nil or empty collections."
-    (is (= [1 2 3 4 5 6 7 8 0] (first (solve [1 2 3, 4 5 0, 6 7 8]))))))
+    (is (= [1 2 3, 4 5 6, 7 8 0] (first (solve [1 2 3, 4 5 0, 6 7 8]))))))
 
 (deftest solution-space-test
   (testing "It walks through the solution space and constructs a shortest path."
-    (let [initial-state [1 2 3 4 5 6 0 7 8]
-          expected-path [[1 2 3 4 5 6 0 7 8]
-                         [1 2 3 4 5 6 7 0 8]
-                         [1 2 3 4 5 6 7 8 0]]
-          solution-space {[1 2 3 4 0 6 7 5 8] [1 2 3 4 5 6 7 0 8], 
-                          [1 2 3 4 6 0 7 5 8] [1 2 3 4 0 6 7 5 8], 
-                          [1 2 3 5 0 6 4 7 8] [1 2 3 0 5 6 4 7 8], 
-                          [0 2 3 1 5 6 4 7 8] [1 2 3 0 5 6 4 7 8], 
-                          [1 2 3 4 5 6 7 0 8] [1 2 3 4 5 6 0 7 8], 
-                          [1 2 3 0 5 6 4 7 8] [1 2 3 4 5 6 0 7 8], 
-                          [1 2 3 4 5 6 7 8 0] [1 2 3 4 5 6 7 0 8], 
-                          [1 0 3 4 2 6 7 5 8] [1 2 3 4 0 6 7 5 8], 
-                          [1 2 3 0 4 6 7 5 8] [1 2 3 4 0 6 7 5 8]}]
+    (let [initial-state  [1 2 3, 4 5 6, 0 7 8]
+          expected-path [[1 2 3, 4 5 6, 0 7 8]
+                         [1 2 3, 4 5 6, 7 0 8]
+                         [1 2 3, 4 5 6, 7 8 0]]
+          solution-space {[1 2 3, 4 0 6, 7 5 8] [1 2 3, 4 5 6, 7 0 8], 
+                          [1 2 3, 4 6 0, 7 5 8] [1 2 3, 4 0 6, 7 5 8], 
+                          [1 2 3, 5 0 6, 4 7 8] [1 2 3, 0 5 6, 4 7 8], 
+                          [0 2 3, 1 5 6, 4 7 8] [1 2 3, 0 5 6, 4 7 8], 
+                          [1 2 3, 4 5 6, 7 0 8] [1 2 3, 4 5 6, 0 7 8], 
+                          [1 2 3, 0 5 6, 4 7 8] [1 2 3, 4 5 6, 0 7 8], 
+                          [1 2 3, 4 5 6, 7 8 0] [1 2 3, 4 5 6, 7 0 8], 
+                          [1 0 3, 4 2 6, 7 5 8] [1 2 3, 4 0 6, 7 5 8], 
+                          [1 2 3, 0 4 6, 7 5 8] [1 2 3, 4 0 6, 7 5 8]}]
         (is (= (shortest-path solution-space initial-state) expected-path)))))
