7985217ad7e39f3de24e60ec50d5700198be0451
Peter Lucht <hape@quantenparallaxum.de>
2021-01-13 14:48:04 +0100

    add a pretty print helper
    magic numbers for the hacky win

--- src/clj8puzzle/core.clj
+++ src/clj8puzzle/core.clj
@@ -3,7 +3,7 @@
 
 (defn -main
   "I don't do a whole lot ... yet."
-  ; probably something like: 
+  ; probably something like:
   ;   read input
   ;   update state or find shortest path to final state
   ;   output result
@@ -14,3 +14,7 @@
 (def initialState [1 2 3
                    4 5 6
                    7 0 8])
+
+(defn state->str [s]
+  (reduce #(str %1 %2 "\n") ""
+    (map #(subs (clojure.string/join " " s) %1 (+ %1 5)) '(0 6 12))))
--- test/clj8puzzle/core_test.clj
+++ test/clj8puzzle/core_test.clj
@@ -1,3 +1,7 @@
 (ns clj8puzzle.core-test
   (:require [clojure.test :refer :all]
             [clj8puzzle.core :refer :all]))
+
+(deftest pretty-print-test
+  (testing "It transforms the state to a pretty string."
+    (is (= (state->str [1 2 3 4 5 6 7 8 0]) "1 2 3\n4 5 6\n7 8 0\n"))))
