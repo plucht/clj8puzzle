f998f783775d5aed99b8f09af70a28f755fc2456
Peter Lucht <hape@quantenparallaxum.de>
2021-01-13 13:45:00 +0100

    fix indentation in some files

--- test/clj8puzzle/solver_test.clj
+++ test/clj8puzzle/solver_test.clj
@@ -1,51 +1,49 @@
 (ns clj8puzzle.solver-test
-    (:require [clojure.test :refer :all]
-              [clj8puzzle.solver :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clj8puzzle.solver :refer :all]))
 
 (deftest goal-test
-    (testing "Goal test happy case."
-        (is (true? (goal? [1 2 3 4 5 6 7 8 0])))))
+  (testing "Goal test happy case."
+    (is (true? (goal? [1 2 3 4 5 6 7 8 0])))))
 
 (deftest goal-test
-    (testing "Goal test unhappy case."
-        (is (false? (goal? [1 2 3 4 5 6 7 0 8])))))
+  (testing "Goal test unhappy case."
+    (is (false? (goal? [1 2 3 4 5 6 7 0 8])))))
 
 (deftest filter-unexplored-one-arg-test
-    (testing "Only keep nodes which are not queue."
-        (let [nodes 
-              [[1 2] [3 4] [5 6]] 
-              
-              queue '([1 2] [5 6])
-            ] 
-            (is (= '([3 4]) (without nodes queue))))))
+  (testing "Only keep nodes which are not queue."
+    (let [nodes
+          [[1 2] [3 4] [5 6]]
+
+          queue '([1 2] [5 6])]
+          (is (= '([3 4]) (without nodes queue))))))
 
 (deftest filter-unexplored-two-args-test
-    (testing "Only keep nodes which are neither in open list nor closed list."
-        (let [nodes 
-              [[1 2] [3 4] [5 6]] 
-              
-              open-list 
-              (conj clojure.lang.PersistentQueue/EMPTY [1 2] [7 8]) 
-            
-              closed-list 
-              (conj #{} [5 6])
-            ] 
-            (is (= '([3 4]) (without nodes open-list closed-list))))))
+  (testing "Only keep nodes which are neither in open list nor closed list."
+    (let [nodes
+          [[1 2] [3 4] [5 6]]
+
+          open-list
+          (conj clojure.lang.PersistentQueue/EMPTY [1 2] [7 8])
+
+          closed-list
+          (conj #{} [5 6])]
+        (is (= '([3 4]) (without nodes open-list closed-list))))))
 
 (deftest initial-state-is-goal-test
-    (testing "Initial state is already the goal."
-        (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 4 5 6 7 8 0])))))
+  (testing "Initial state is already the goal."
+    (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 4 5 6 7 8 0])))))
 
 (deftest one-move-away-test
-    (testing "Goal is one move away."
-        (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 4 5 6 7 0 8])))
-        (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 4 5 0 7 8 6])))))
+  (testing "Goal is one move away."
+    (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 4 5 6 7 0 8])))
+    (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 4 5 0 7 8 6])))))
 
 (deftest two-moves-away-test
-    (testing "Goal is two moves away."
-        (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 4 5 6 0 7 8])))
-        (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 4 0 5 7 8 6])))))
+  (testing "Goal is two moves away."
+    (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 4 5 6 0 7 8])))
+    (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 4 0 5 7 8 6])))))
 
 (deftest three-moves-away-test
-    (testing "Goal is three moves away."
-        (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 0 4 5 7 8 6])))))
+  (testing "Goal is three moves away."
+    (is (= [1 2 3 4 5 6 7 8 0] (solve [1 2 3 0 4 5 7 8 6])))))
--- test/clj8puzzle/state_test.clj
+++ test/clj8puzzle/state_test.clj
@@ -1,56 +1,56 @@
 (ns clj8puzzle.state-test
     (:require [clojure.test :refer :all]
               [clj8puzzle.state :refer :all]))
-  
-  (deftest move-left-test
-    (testing "Impossible moves should result in nil."
-      (is (nil? (move-left [0 1 2 3 4 5 6 7 8])))
-      (is (nil? (move-left [1 2 3 0 4 5 6 7 8])))
-      (is (nil? (move-left [1 2 3 4 5 6 0 7 8]))))
-    
-    (testing "Possible moves should result in a new vector."
-      (is (= [1 2 3 4 5 6 7 0 8] (move-left [1 2 3 4 5 6 7 8 0])))
-      (is (= [0 1 2 3 4 5 6 7 8] (move-left [1 0 2 3 4 5 6 7 8])))))
-  
-  (deftest move-right-test
-    (testing "Impossible moves should result in nil."
-      (is (nil? (move-right [1 2 3 4 5 6 7 8 0])))
-      (is (nil? (move-right [1 2 3 4 5 0 6 7 8])))
-      (is (nil? (move-right [1 2 0 3 4 5 6 7 8]))))
-    
-    (testing "Possible moves should result in a new vector."
-      (is (= [1 2 3 4 5 6 7 8 0] (move-right [1 2 3 4 5 6 7 0 8])))
-      (is (= [1 0 2 3 4 5 6 7 8] (move-right [0 1 2 3 4 5 6 7 8])))))
-  
-  (deftest move-up-test
-    (testing "Impossible moves should result in nil."
-      (is (nil? (move-up [0 1 2 3 4 5 6 7 8])))
-      (is (nil? (move-up [1 0 2 3 4 5 6 7 8])))
-      (is (nil? (move-up [1 2 0 3 4 5 6 7 8]))))
-    
-    (testing "Possible moves should result in a new vector."
-      (is (= [0 2 3 1 4 5 6 7 8] (move-up [1 2 3 0 4 5 6 7 8])))
-      (is (= [1 2 3 4 5 0 7 8 6] (move-up [1 2 3 4 5 6 7 8 0])))))
-  
-  (deftest move-down-test
-    (testing "Impossible moves should result in nil."
-      (is (nil? (move-down [1 2 3 4 5 6 7 8 0])))
-      (is (nil? (move-down [1 2 3 4 5 6 7 0 8])))
-      (is (nil? (move-down [1 2 3 4 5 6 0 7 8]))))
-    
-    (testing "Possible moves should result in a new vector."
-      (is (= [1 2 3 0 4 5 6 7 8] (move-down [0 2 3 1 4 5 6 7 8])))
-      (is (= [1 2 3 4 5 6 7 8 0] (move-down [1 2 3 4 5 0 7 8 6])))))
-  
-  (deftest generate-states-test
-    (testing "When moving from any corner, exactly two states can be generated."
-      ; find a way to check if both expected states are in the list of states
-      ; returned by 'generate-states', independent of their order. 
-      ; That's why I currently only check the length instead of a concrete 
-      ; list with two vectors. That would bind the test case to rely on this
-      ; order. This would be okay, if the order would matter, but the order doesn't
-      ; matter, so it would be dumb to expect a certain order in the test case. 
-      (is (= 2 (count (generate-states [0 1 2 3 4 5 6 7 8]))))
-      (is (= 2 (count (generate-states [1 2 0 3 4 5 6 7 8]))))
-      (is (= 2 (count (generate-states [1 2 3 4 5 6 7 8 0]))))
-      (is (= 2 (count (generate-states [1 2 3 4 5 6 0 7 8]))))))
+
+(deftest move-left-test
+  (testing "Impossible moves should result in nil."
+    (is (nil? (move-left [0 1 2 3 4 5 6 7 8])))
+    (is (nil? (move-left [1 2 3 0 4 5 6 7 8])))
+    (is (nil? (move-left [1 2 3 4 5 6 0 7 8]))))
+
+  (testing "Possible moves should result in a new vector."
+    (is (= [1 2 3 4 5 6 7 0 8] (move-left [1 2 3 4 5 6 7 8 0])))
+    (is (= [0 1 2 3 4 5 6 7 8] (move-left [1 0 2 3 4 5 6 7 8])))))
+
+(deftest move-right-test
+  (testing "Impossible moves should result in nil."
+    (is (nil? (move-right [1 2 3 4 5 6 7 8 0])))
+    (is (nil? (move-right [1 2 3 4 5 0 6 7 8])))
+    (is (nil? (move-right [1 2 0 3 4 5 6 7 8]))))
+
+  (testing "Possible moves should result in a new vector."
+    (is (= [1 2 3 4 5 6 7 8 0] (move-right [1 2 3 4 5 6 7 0 8])))
+    (is (= [1 0 2 3 4 5 6 7 8] (move-right [0 1 2 3 4 5 6 7 8])))))
+
+(deftest move-up-test
+  (testing "Impossible moves should result in nil."
+    (is (nil? (move-up [0 1 2 3 4 5 6 7 8])))
+    (is (nil? (move-up [1 0 2 3 4 5 6 7 8])))
+    (is (nil? (move-up [1 2 0 3 4 5 6 7 8]))))
+
+  (testing "Possible moves should result in a new vector."
+    (is (= [0 2 3 1 4 5 6 7 8] (move-up [1 2 3 0 4 5 6 7 8])))
+    (is (= [1 2 3 4 5 0 7 8 6] (move-up [1 2 3 4 5 6 7 8 0])))))
+
+(deftest move-down-test
+  (testing "Impossible moves should result in nil."
+    (is (nil? (move-down [1 2 3 4 5 6 7 8 0])))
+    (is (nil? (move-down [1 2 3 4 5 6 7 0 8])))
+    (is (nil? (move-down [1 2 3 4 5 6 0 7 8]))))
+
+  (testing "Possible moves should result in a new vector."
+    (is (= [1 2 3 0 4 5 6 7 8] (move-down [0 2 3 1 4 5 6 7 8])))
+    (is (= [1 2 3 4 5 6 7 8 0] (move-down [1 2 3 4 5 0 7 8 6])))))
+
+(deftest generate-states-test
+  (testing "When moving from any corner, exactly two states can be generated."
+    ; find a way to check if both expected states are in the list of states
+    ; returned by 'generate-states', independent of their order.
+    ; That's why I currently only check the length instead of a concrete
+    ; list with two vectors. That would bind the test case to rely on this
+    ; order. This would be okay, if the order would matter, but the order doesn't
+    ; matter, so it would be dumb to expect a certain order in the test case.
+    (is (= 2 (count (generate-states [0 1 2 3 4 5 6 7 8]))))
+    (is (= 2 (count (generate-states [1 2 0 3 4 5 6 7 8]))))
+    (is (= 2 (count (generate-states [1 2 3 4 5 6 7 8 0]))))
+    (is (= 2 (count (generate-states [1 2 3 4 5 6 0 7 8]))))))
