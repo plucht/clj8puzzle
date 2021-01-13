88f288fd322f08faad0f8d7694e08aabb63d963c
Peter Lucht <hape@quantenparallaxum.de>
2021-01-13 15:33:44 +0100

    fix identation and whitespace in solver.clj

--- src/clj8puzzle/solver.clj
+++ src/clj8puzzle/solver.clj
@@ -1,42 +1,39 @@
 (ns clj8puzzle.solver
-    (:require [clojure.set :refer [difference]]
-              [clj8puzzle.state :refer [generate-states]])
-    (:gen-class))
-  
-(defn goal? 
-    [state]
-    "Check if state is the desired final state of the game." 
-    (= state [1 2 3 4 5 6 7 8 0]))
+  (:require [clojure.set :refer [difference]]
+            [clj8puzzle.state :refer [generate-states]])
+  (:gen-class))
 
-(defn without 
-    [collection & others] 
-    "Get collection without values present in any other given collection."
-    (seq 
-        (difference 
-            (set collection) 
-            (reduce #(into %1 %2) #{} (map set others)))))
+(defn goal?
+  [state]
+  "Check if state is the desired final state of the game."
+  (= state [1 2 3 4 5 6 7 8 0]))
+
+(defn without
+  [collection & others]
+  "Get collection without values present in any other given collection."
+  (seq
+    (difference
+      (set collection)
+      (reduce #(into %1 %2) #{} (map set others)))))
 
 (defn solve [state]
-    (loop [open-list (conj clojure.lang.PersistentQueue/EMPTY state)
-           closed-list #{}]
-            (if (goal? (peek open-list)) (peek open-list) 
-                (if (empty? open-list) nil ; no solution
-                (let [node 
-                      (peek open-list)
-                      
-                      generated-nodes 
-                      (generate-states node)
-                      
-                      unexplored-nodes 
-                      (without generated-nodes closed-list open-list)
-                      
-                      updated-open-list 
-                      (apply conj (pop open-list) unexplored-nodes)
-                      
-                      updated-closed-list 
-                      (apply conj closed-list node (without generated-nodes unexplored-nodes))
-                     ]
-                    (recur updated-open-list updated-closed-list)
-                ))
-            )       
-    ))
+  (loop [open-list (conj clojure.lang.PersistentQueue/EMPTY state)
+         closed-list #{}]
+        (if (goal? (peek open-list)) (peek open-list)
+          (if (empty? open-list) nil ; no solution
+            (let [node
+                  (peek open-list)
+
+                  generated-nodes
+                  (generate-states node)
+
+                  unexplored-nodes
+                  (without generated-nodes closed-list open-list)
+
+                  updated-open-list
+                  (apply conj (pop open-list) unexplored-nodes)
+
+                  updated-closed-list
+                  (apply conj closed-list node (without generated-nodes unexplored-nodes))
+                  ]
+                  (recur updated-open-list updated-closed-list))))))
